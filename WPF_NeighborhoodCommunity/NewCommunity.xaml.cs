2f089bf0194507f8498e9d636487d71e50cbb89c
Miguel Gomez <miguel5121996@gmail.com>
2024-01-17 19:34:26 +0100

    Realizo primer Wireframe Crear Comunidad

---
+++ WPF_NeighborhoodCommunity/NewCommunity.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_NeighborhoodCommunity
+{
+    /// <summary>
+    /// Lógica de interacción para NewCommunity.xaml
+    /// </summary>
+    public partial class NewCommunity : Window
+    {
+        public NewCommunity()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
